ad057c07e9222833e712c25405618f74227eb019
aldijanaa <116015661+aldijanaa@users.noreply.github.com>
2022-10-27 00:13:07 +0200

    Add files via upload

---
+++ Main/src/main/java/com/mycompany/main/Main.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.main;
+
+/**
+ *
+ * @author aldijana
+ */
+//import java.util.Scanner;
+//import static java.lang.Math.*;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        //task 1
+        // Scanner scanner = new Scanner(System.in);
+        //String password = "carrot";
+
+        /*while (true){
+            System.out.println(" Enter your password: ");  //asking user for input
+            String guess = scanner.nextLine();
+
+            if(guess.equals(password)){
+                System.out.println("Right!");
+                System.out.println("The secret is: " + password);
+                break;
+            }
+            else{
+                System.out.println("Wrong! Try again");
+            }
+
+        } */
+
+        //task 2
+        /*int sum = 0;
+        int read;
+        System.out.println("Enter first number : ");
+        read = scanner.nextInt();
+        sum = sum + read;
+        System.out.println("Enter second number : ");
+        read = scanner.nextInt();
+        sum = sum + read;
+        System.out.println("Enter third number : ");
+        read = scanner.nextInt();
+        sum = sum + read;
+
+        System.out.println("Sum is: " + sum); */
+
+
+        //task 3
+        /*int sum = 0;
+        while (true){
+            int read = scanner.nextInt();
+            if(read==0){
+                break;
+            }else{
+                sum = sum + read;
+                System.out.println("Sum now: " + sum);
+            }
+        }
+        System.out.println("sum in the end is: " + sum);*/
+
+        //task 4
+        /*System.out.println("enter first number: ");
+        int first = scanner.nextInt();
+        System.out.println("enter last number: ");
+        int last = scanner.nextInt();
+
+        while( first <= last){
+            System.out.println(first);
+            first++;
+        } */
+
+
+         //task 5
+        /* System.out.println(" enter your power: ");
+        int pow = scanner.nextInt();
+        int sum = 0;
+
+        for(int i = 0; i<=pow; i++){
+            sum = sum + (int)Math.pow(2,i);
+        }
+        System.out.println("The result is:" + sum); */
+
+        //task 6
+        /*int times = scanner.nextInt(); */
+        //printText(times);
+        //printStars(times);
+
+        //task 7
+
+    }
+    /*public static void printText(int times){
+        for(int i = 0; i<times; i++){
+            System.out.println("In the beginning there were the swamp, the hoe and java ");
+        }*/
+
+    }
+
+    /*public static void printStars(int times){
+        for(int i = 0; i< times; i++){
+            System.out.println("*");
+        }*/
+
+    }
+
+
+
+}
